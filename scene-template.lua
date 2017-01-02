f8661f5187b54bc289304c34f16c8822815ea2b9
John Chin-Jew <jchinjew@gmail.com>
2017-01-02 01:18:37 -0800

    Comment clarification

--- scene-template.lua
+++ scene-template.lua
@@ -1,50 +1,62 @@
 --
--- Localities.
+-- Imports.
 --
-
 local composer = require("composer")
 local scene = composer.newScene()
+-- ...
+
+
 
 --
--- Scene events.
+-- Variables, globals, etc to execute only ONCE.
 --
+-- ...
 
--- Runs ONCE at scene creation before visible.
+
+
+--
+-- Runs ONCE at EACH scene creation before visible.
+--
 function scene:create(event)
-
   local sceneGroup = self.view
-
-  -- pause physics
+  
+  -- define groups
+  -- pause physics if started
   -- define display objects
-  -- insert objects into scene view
+  -- insert objects/groups into scene view
   -- load scene specific audio
-  -- touch/tap handlers
-
+  -- touch handlers
+  -- ...
 end
 
--- will: Runs before screen shows.
--- did: Runs after screen shows.
-function scene:show(event)
 
+
+--
+-- Runs before & after screen shows.
+--
+function scene:show(event)
   local sceneGroup = self.view
   local phase = event.phase
 
   if (phase == "will") then
     -- position elements
+    -- ...
 
   elseif (phase == "did") then
     -- timers, transitions, animations
     -- start physics
     -- play scene specific audio
     -- Runtime event listeners (enterFrame, etc)
+    -- ...
   end
-
 end
 
--- will: Runs before screen hides.
--- did: Runs after screen hides.
-function scene:hide(event)
 
+
+--
+-- Runs before & after screen hides.
+--
+function scene:hide(event)
   local sceneGroup = self.view
   local phase = event.phase
 
@@ -53,29 +65,34 @@ function scene:hide(event)
     -- remove Runtime listeners (enterFrame, etc)
     -- pause physics
     -- stop audio
+    -- stop timers
+    -- ...
 
   elseif (phase == "did") then
     -- optional: force screen removal
+    -- ...
   end
-
 end
 
--- Runs directly before scene removal.
-function scene:destroy(event)
 
+
+--
+-- Runs directly before scene removal.
+--
+function scene:destroy(event)
   local sceneGroup = self.view
 
   -- destroy scene specific audio
-
+  -- ...
 end
 
---
--- Scene event listeners.
---
 
+
+--
+-- End.
+--
 scene:addEventListener("create", scene)
 scene:addEventListener("show", scene)
 scene:addEventListener("hide", scene)
 scene:addEventListener("destroy", scene)
-
 return scene
